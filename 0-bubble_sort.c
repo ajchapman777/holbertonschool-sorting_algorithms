8d4b7542020dc9a6e21004e84fb04bec9f96289d
ajchapman777 <ajchapman777@gmail.com>
2023-12-18 21:08:25 -0800

    corrected Betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,10 +6,10 @@
  */
 void swap_ints(int *a, int *b)
 {
-        int tmp;
-        tmp = *a;
-        *a = *b;
-        *b = tmp;
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 /**
  * bubble_sort - Sort an array of integers in ascending order.
@@ -20,22 +20,24 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, len = size;
-        bool bubbly = false;
-        if (array == NULL || size < 2)
-                return;
-        while (bubbly == false)
-        {
-                bubbly = true;
-                for (i = 0; i < len - 1; i++)
-                {
-                        if (array[i] > array[i + 1])
-                        {
-                                swap_ints(array + i, array + i + 1);
-                                print_array(array, size);
-                                bubbly = false;
-                        }
-                }
-                len--;
-        }
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+	while (bubbly == false)
+	{
+		bubbly = true;
+
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
 } 
